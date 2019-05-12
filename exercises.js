47e446e8602974a8bd2d35581f6e5a56276ad94b
Janelle K Hirano <jaahnelle@gmail.com>
2019-05-12 09:00:04 -1000

    added function login

--- exercises.js
+++ exercises.js
@@ -50,6 +50,13 @@ console.log('canVote: ', canVote(16));
  * Console.log your result.
 */
 
+function login(password){
+  if (password === 'test1234'){
+    return "Login Success!";
+  }
+}
+
+console.log('login: ', login('test1234'));
 
 /*
  * #3
